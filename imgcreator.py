81486913dc3120df852b59271d92d8172ecbf2fd
Jillian Handrahan <jillianhandrahan@gmail.com>
2024-04-25 20:35:30 -0400

    Finished imgcreator version 1, creates n jpeg or tiff files at random numbered 1 to n, stores in folder named by the user.
    Fixed image sorting in imgreader, sort images after entire list is created

--- imgcreator.py
+++ imgcreator.py
@@ -1,12 +1,49 @@
-
-def create_images():
-    print("create")
-
-
+import os
+import random
+from PIL import Image, ImageDraw, ImageFont
 
 
+def create_images(folder_name, n):
+    # Create the folder if it doesn't exist
+    os.chdir('/Volumes/PHILIPS UFD')
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+
+    # Supported image formats
+    image_formats = ['jp2', 'tif']
+
+    # Calculate image size in pixels (assuming 300 ppi)
+    width_pixels = int(8.5 * 300)
+    height_pixels = int(3362)
+
+    for i in range(1, n + 1):
+        # Generate random file name
+        file_name = f"IMG_{i}.{random.choice(image_formats)}"
+
+        # Generate the image with its number on it
+        image = Image.new('RGB', (width_pixels, height_pixels), color='white')
+        draw = ImageDraw.Draw(image)
+        font = ImageFont.load_default(100)
+        #vfont.size = 50
+        text = str(i)
+        draw.text(((width_pixels - 60) // 2, (height_pixels - 60) // 2), text, fill='black', font=font)
+
+        # Save the image
+        image.save(os.path.join(folder_name, file_name))
+
+    print(f"{n} images created in '{folder_name}' folder.")
 
 
+def main():
+    # Get folder name from user
+    folder_name = input("Enter folder name to store images: ")
 
-if __name__ == '__main__':
-    create_images()
+    # Get number of images from user
+    n = int(input("Enter number of images to create: "))
+
+    # Create images
+    create_images(folder_name, n)
+
+
+if __name__ == "__main__":
+    main()
--- imgreader.py
+++ imgreader.py
@@ -3,27 +3,35 @@ import os
 import sys
 import img2pdf
 
+
+def extract_number(filename):
+    return int(''.join(filter(str.isdigit, filename)))
+
 def read_files_better():
+    #dir = '/Users/jillianhandrahan/steiner_001_images'
+    dir = '/Volumes/PHILIPS UFD'
     # convert all files ending in .jpg inside a directory
     print("Welcome. Some files may be skipped. Normal skipped files: .DS_Store, IMG folder names like IMG001")
-    os.chdir('/Users/jillianhandrahan/target_images')
-    #dirname = os.getcwd() + '/IMAGES'
-    #print("Current working directory:", dirname)
-    print("Current working directory: /Users/jillianhandrahan/target_images")
+    os.chdir(dir)
+    # dirname = os.getcwd() + '/IMAGES'
+    # print("Current working directory:", dirname)
+    print("Current working directory: ", dir)
     created = []
 
-    for dirpath, dirname, filenames in os.walk('/Users/jillianhandrahan/target_images'):
+    for dirpath, dirname, filenames in os.walk(dir):
         imgs = []
         for fname in os.listdir(dirpath):
-            if fname != ".DS_Store" and (fname.endswith(".jpg") or fname.endswith(".tif")):
+            if (not fname.startswith(".")) and fname != ".DS_Store" \
+                    and (fname.endswith(".jpg") or fname.endswith(".tif")):
+                print("filename: ", fname)
                 path = os.path.join(dirpath, fname)
                 if os.path.isdir(path):
                     continue
                 imgs.append(path)
-                imgs.sort()
             else:
                 print("Skipped a file. File name: " + fname)
                 continue
+        imgs = sorted(imgs, key=extract_number)
         if len(imgs) != 0:
             filename = os.path.basename(os.path.normpath(dirpath)) + '.pdf'
             created.append(filename)
@@ -33,38 +41,7 @@ def read_files_better():
     for name in created:
         print(name)
 
-#hi
-# def read_files():
-#     # convert all files ending in .jpg inside a directory
-#     print("Welcome. Some files may be skipped. Normal skipped files: .DS_Store, IMG folder names like IMG001")
-#     os.chdir('/Users/jillianhandrahan/target_images')
-#     dirname = os.getcwd() + '/IMAGES'
-#     created = []
-#
-#     for dirpath, dirname, filenames in os.walk(dirname):
-#         imgs = []
-#         for fname in os.listdir(dirpath):
-#             if fname != ".DS_Store" and (fname.endswith(".jpg") or fname.endswith(".tif")):
-#                 path = os.path.join(dirpath, fname)
-#                 if os.path.isdir(path):
-#                     continue
-#                 imgs.append(path)
-#                 imgs.sort()
-#             else:
-#                 print("Skipped a file. File name: " + fname)
-#                 continue
-#         if len(imgs) != 0:
-#             filename = os.path.basename(os.path.normpath(dirpath)) + '.pdf'
-#             created.append(filename)
-#             with open(filename, "wb") as f:
-#                 f.write(img2pdf.convert(imgs))
-#     print("SUCCESS: " + str(len(created)) + " PDF files created: ")
-#     for name in created:
-#         print(name)
-
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-     #read_files()
     read_files_better()
-
